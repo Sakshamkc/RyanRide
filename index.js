f495952d303580c689bc5188719fae94baa041b0
saksham.kc <saksham.kc@cotiviti.com>
2025-07-08 15:14:02 +0000

    active state checked

--- index.js
+++ index.js
@@ -60,4 +60,25 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('Form validation failed.');
         }
     });
+
+    const sections = document.querySelectorAll("section[id]");
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+
+        sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 100;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+        });
+
+        navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+        });
+    });
 });
